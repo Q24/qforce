5761edc848b56fd609af545a0e139644678ee686
ChrisVaessens <chris.vaessens@gmail.com>
2024-10-21 10:44:01 +0200

    Added OpenAPI

--- src/main/java/nl/qnh/qforce/QforceApplication.java
+++ src/main/java/nl/qnh/qforce/QforceApplication.java
@@ -3,7 +3,17 @@ package nl.qnh.qforce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Ilionx QForce",
+        version = "1.0",
+        description = "This API will return Star Wars characters"
+    )
+)
 public class QforceApplication {
 
 	public static void main(String[] args) {
